7cced16c86354752996f7d48d70e93ce7530abd0
flavianna <flaviannabe@gmail.com>
2023-04-20 01:00:05 -0300

    fix: erros de lint

--- src/service/post.service.js
+++ src/service/post.service.js
@@ -6,11 +6,13 @@ const createNewPost = async ({ title, content, categoryIds }, userEmail) => {
     const user = await User.findOne({ where: { email: userEmail } });
     const userId = user.dataValues.id;
     const post = await BlogPost.create({ title, content, userId });
-    
-    const postCategories = categories.map(category => ({ 
-        postId: post.id, categoryId: category.id }));
+
+    const postCategories = categories.map((category) => ({
+        postId: post.id,
+        categoryId: category.id
+    }));
     await PostCategory.bulkCreate(postCategories);
-    
+
     return post;
 };
 
